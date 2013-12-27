13052356c8d50c90a1744faf538c5523add706a8
jonasbn <jonasbn@logicLAB.dk>
2013-12-27 09:24:58 +0100

    Added a paragraph on possible bugs and limitations in regard to RT:91542 and Net::OpenSSH

--- lib/Perl/Critic/Policy/logicLAB/ProhibitShellDispatch.pm
+++ lib/Perl/Critic/Policy/logicLAB/ProhibitShellDispatch.pm
@@ -141,6 +141,13 @@ This distribution has no known incompatibilities.
 
 This distribution has no known bugs or limitations.
 
+As pointed out in bug report RT:91542, some modules and components might 
+implement methods/routines holding names similar to the builtins system 
+and exec. I had not anticipated this when first implementing the policy 
+and there might be some cases where the current implementation does not 
+handle these well, please file a bugreport if you run into one of these 
+and I will address these.
+
 =head1 BUG REPORTING
 
 Please use Requets Tracker for bug reporting:
